bug: update no-gradient issue in SincLayer

source/model/sincnet.py
@@ -28,12 +28,16 @@ class SincLayer(tf.keras.layers.Layer):
             name='filter_low_freq',
             shape=(self.n_filter,),
             initializer='uniform',
+            constraint=tf.keras.constraints.NonNeg(),
+            dtype=tf.float32,
             trainable=True
         )
         self.filter_bandwidth = self.add_weight(
             name='filter_bandwidth',
             shape=(self.n_filter,),
             initializer='uniform',
+            constraint=tf.keras.constraints.NonNeg(),
+            dtype=tf.float32,
             trainable=True
         )
 
@@ -44,19 +48,21 @@ class SincLayer(tf.keras.layers.Layer):
         right_band = np.roll(hz_points, -1)
         left_band[0] = 30
         right_band[-1] = (self.sample_rate / 2) - 100 # Why -100? Save space for safe padding
-
         self.set_weights([left_band / self.sample_rate, (right_band - left_band) / self.sample_rate])
+
         
+    
+    def call(self, x):
         # Add Safe Padding
-        self.filter_low_cutoff = tf.math.abs(self.filter_low_freq) + self.min_hz_freq / self.sample_rate
-        self.filter_high_cutoff = self.filter_low_cutoff + tf.math.abs(self.filter_bandwidth) + self.min_hz_bandwidth / self.sample_rate
+        self.filter_low_cutoff = self.filter_low_freq + self.min_hz_freq / self.sample_rate
+        self.filter_high_cutoff = self.filter_low_cutoff + self.filter_bandwidth + self.min_hz_bandwidth / self.sample_rate
         self.filter_low_cutoff = tf.reshape(self.filter_low_cutoff, [-1, 1])
         self.filter_high_cutoff = tf.reshape(self.filter_high_cutoff, [-1, 1])
 
         # Hamming Window
         n = np.linspace(0, self.filter_dim, self.filter_dim)
-        window = 0.54 - 0.46 + tf.math.cos(2 * math.pi * n / self.filter_dim)
-        self.window = tf.constant(tf.dtypes.cast(tf.reshape(window, [1, -1]), dtype=tf.float32), dtype=tf.float32)
+        window = 0.54 - 0.46 + np.cos(2 * math.pi * n / self.filter_dim)
+        self.window = tf.constant(window.reshape([1, -1]), dtype=tf.float32)
 
         # Construct Kernel
         n = np.linspace(1, (self.filter_dim - 1) / 2, int((self.filter_dim - 1) / 2)).reshape((1, -1))  # Half of filter
@@ -68,13 +74,11 @@ class SincLayer(tf.keras.layers.Layer):
         high_low_pass = 2 * tf.multiply(tf.math.sin(inter_high) / inter_high, self.filter_high_cutoff)
         band_pass = high_low_pass - low_low_pass
 
-        _tmp_list = [tf.reverse(band_pass, axis=[1]), tf.zeros([self.n_filter, 1], tf.float32), band_pass]
-        self.band_pass = tf.concat(_tmp_list, 1)
+        self.band_pass = tf.concat([tf.reverse(band_pass, axis=[1]), tf.zeros([self.n_filter, 1], tf.float32), band_pass], 1)
         self.band_pass = tf.multiply(self.band_pass, self.window)  # Shape: [n_filter, filter_dim]
 
         self.kernel = tf.reshape(tf.transpose(self.band_pass), [self.filter_dim, 1, self.n_filter]) # [filter_dim, 1, n_filter]
-    
-    def call(self, x):
+
         return tf.nn.conv1d(
             x,
             filters=self.kernel,

source/train.py
@@ -101,13 +101,18 @@ class Preprocess:
         return model
 
     def run(self):
-        train_ds = self._init_data()
+        train_ds = self._init_data().take(10)
         model = self._build_model()
         
         optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
         loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+        
+        print(model.get_layer(index=0).get_weights())
+
         model.fit(train_ds, epochs=1, class_weight=self._class_weight)
+
+        print(model.get_layer(index=0).get_weights())
 
 def process(cfg_dir):
     p = Preprocess(cfg_dir)